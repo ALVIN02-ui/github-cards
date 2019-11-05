6908ff254645d9b467cb6c3070f8dd36068ec115
Ivy Walobwa <ivywalobwa@gmail.com>
2019-11-05 16:05:00 +0300

    modified files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-intro'`, () => {
+  it(`should have as title 'github-cards'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-intro');
+    expect(app.title).toEqual('github-cards');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-intro app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('github-cards app is running!');
   });
 });
